6be387f3130b9609b0747398153cf4caf8f0fadd
i.yuji.shibata <i.yuji.shibata@globis.co.jp>
2022-10-18 11:15:11 +0900

    objectの要素数を見るときは、Object.keys(hoge).length

--- src/Code.ts
+++ src/Code.ts
@@ -69,19 +69,15 @@ class SimpleHttpClient {
     };
 
     request(url: string, method: GoogleAppsScript.URL_Fetch.HttpMethod, payload?: GoogleAppsScript.URL_Fetch.Payload, headers?: GoogleAppsScript.URL_Fetch.HttpHeaders): GoogleAppsScript.URL_Fetch.HTTPResponse {
-        console.log(`request url: ${url}`)
-        console.log(`payload: ${JSON.stringify(payload)}`)
-        console.log(`headers: ${JSON.stringify(headers)}`)
-        console.log(`cookies: ${JSON.stringify(this.cookies)}`)
         const options: URLFetchRequestOptions = {
             method: method,
             headers: headers,
             payload: payload,
             followRedirects: false,
         }
-        if (this.cookies.length > 0) {
+        if (Object.keys(this.cookies).length > 0) {
             options.headers = options.headers || {}
-            options.headers['Cookie'] = Object.keys(this.cookies).map((k: string) => `${k}=${this.cookies[k]}`).join(';')
+            options.headers['Cookie'] = Object.keys(this.cookies).map(key => `${key}=${this.cookies[key]}`).join('; ')
         }
         const response = UrlFetchApp.fetch(url, options)
         const responseHeaders: { [key: string]: any } = response.getAllHeaders()
