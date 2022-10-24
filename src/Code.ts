5770293c987046dca6077ce8cb73c8781bee73cf
i.yuji.shibata <i.yuji.shibata@globis.co.jp>
2022-10-24 11:22:30 +0900

    sheetの定数化、部分更新の範囲指定機能の追加

--- src/Code.ts
+++ src/Code.ts
@@ -6,6 +6,8 @@ import {lastScenarioExecute, relationPlans} from "./ScenarioScraping";
 import AUTIFY_API_URL = Constants.AUTIFY_API_URL;
 import START_BODY_ROW = Constants.START_BODY_ROW;
 
+const SHEET = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('scenarios') || SpreadsheetApp.getActiveSpreadsheet().insertSheet('scenarios')
+
 const createOptions = () => {
     const bearerKey = PropertiesService.getScriptProperties().getProperty("KEY")
     const headers: GoogleAppsScript.URL_Fetch.HttpHeaders = {
@@ -34,39 +36,65 @@ const getScenario = (id: number) => {
 
 const update = () => {
     const ui = SpreadsheetApp.getUi()
-    const button = ui.alert('シナリオ更新・取得', '実行しますか', ui.ButtonSet.YES_NO)
+    const button = ui.alert('シナリオ 更新・取得', '実行しますか', ui.ButtonSet.YES_NO)
     if (button !== ui.Button.YES) return
-    let page = 1
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('scenarios')
-    if (sheet === null) return
-    const currentValues = sheet.getSheetValues(START_BODY_ROW, 1, sheet.getLastRow(), sheet.getLastColumn())
+    const currentValues = SHEET.getSheetValues(START_BODY_ROW, 1, SHEET.getLastRow(), SHEET.getLastColumn())
     const client = new SimpleHttpClient()
     oauth(client)
+    let page = 1
     while (true) {
         const scenarios = getScenarios(page)
         if (scenarios.length === 0) return
-        scenarios.forEach(s => {
-            writeScenario(currentValues, new Scenario(s), client, sheet)
-        })
+        scenarios.forEach(s => writeScenario(currentValues, new Scenario(s), client))
         page++
     }
 }
 
+const REGEX_ID = /^\d+$/
+const REGEX_RANGE = /^\s*(\d*)\s*(<=*)\s*(\d*)\s*$/
+
 const partialUpdate = () => {
     const ui = SpreadsheetApp.getUi()
-    const promptResponse = ui.prompt('シナリオID指定更新・取得', 'シナリオIDを入力してください', ui.ButtonSet.OK)
+    const promptResponse = ui.prompt('シナリオID指定または範囲指定 更新・取得', 'IDまたは範囲を指定ください(ex. 100000 <= 200000),(ex. 30)', ui.ButtonSet.OK)
     if (promptResponse.getSelectedButton() == ui.Button.CLOSE) return
-    const id = promptResponse.getResponseText()
-    const scenario = getScenario(parseInt(id))
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('scenarios')
-    if (sheet === null) return
-    const currentValues = sheet.getSheetValues(START_BODY_ROW, 1, sheet.getLastRow(), sheet.getLastColumn())
-    const client = new SimpleHttpClient()
-    oauth(client)
-    writeScenario(currentValues, new Scenario(scenario), client, sheet, true)
+    const rangeOrId = promptResponse.getResponseText()
+    if (rangeOrId.match(REGEX_ID)) {
+        singleUpdate(parseInt(rangeOrId))
+    } else if (REGEX_RANGE.test(rangeOrId)) {
+        const regexpResult = REGEX_RANGE.exec(rangeOrId) || []
+        const start = regexpResult[1] ? parseInt(regexpResult[1]) : 0
+        const end = parseInt(regexpResult[3]) ? parseInt(regexpResult[3]) : 99999999
+        const sign = regexpResult[2]
+        if (start <= end) {
+            const currentValues = SHEET.getSheetValues(START_BODY_ROW, 1, SHEET.getLastRow(), SHEET.getLastColumn())
+            const client = new SimpleHttpClient()
+            oauth(client)
+            let page = 1
+            while (true) {
+                const scenarios = getScenarios(page)
+                if (scenarios.length === 0) return
+                scenarios
+                    .filter((s: Scenario) => eval(`${start} ${sign} ${s.id} && ${s.id} ${sign} ${end}`))
+                    .forEach(s => writeScenario(currentValues, new Scenario(s), client))
+                page++
+            }
+        }else{
+            ui.alert('シナリオID指定または範囲指定 更新・取得', '範囲指定が不正です', ui.ButtonSet.OK)
+        }
+    }else{
+        ui.alert('シナリオID指定または範囲指定 更新・取得', '範囲指定が不正です', ui.ButtonSet.OK)
+    }
 }
 
-const writeScenario = (currentValues: any[][], scenario: Scenario, client: SimpleHttpClient, sheet: GoogleAppsScript.Spreadsheet.Sheet, forceUpdate?:boolean) => {
+const singleUpdate = (id: number) => {
+    const scenario = getScenario(id)
+    const currentValues = SHEET.getSheetValues(START_BODY_ROW, 1, SHEET.getLastRow(), SHEET.getLastColumn())
+    const client = new SimpleHttpClient()
+    oauth(client)
+    writeScenario(currentValues, new Scenario(scenario), client, true)
+}
+
+const writeScenario = (currentValues: any[][], scenario: Scenario, client: SimpleHttpClient, forceUpdate?: boolean) => {
     const {
         lastScenarioExecuteDate,
         lastScenarioExecuteLink,
@@ -77,15 +105,13 @@ const writeScenario = (currentValues: any[][], scenario: Scenario, client: Simpl
     const index = currentValues.findIndex(v => v[0] == scenarioWithExecuteResult.id)
     console.info(`target scenario id: ${scenarioWithExecuteResult.id}`)
     if (index < 0)
-        writingScenario(client, scenarioWithExecuteResult, sheet.getLastRow() + 1)
+        writingScenario(client, scenarioWithExecuteResult, SHEET.getLastRow() + 1)
     else if (!scenarioWithExecuteResult.isSame(currentValues[index]) || forceUpdate)
         writingScenario(client, scenarioWithExecuteResult, index + START_BODY_ROW)
 };
 
 const writingScenario = (client: SimpleHttpClient, scenarioWithExecuteResult: ScenarioWithExecuteResult, row: number) => {
-    const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('scenarios')
-    if (sheet === null) return
-    const range = sheet.getRange(row, 1, 1, sheet.getLastColumn())
+    const range = SHEET.getRange(row, 1, 1, SHEET.getLastColumn())
     console.info(`update scenario id: ${scenarioWithExecuteResult.id}`)
     range.setRichTextValues([scenarioWithExecuteResult.toRichTextValues()])
 }
